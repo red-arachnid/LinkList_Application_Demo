0b8943624ccf32d80434d8eed65343d38caa0699
Aniket Yadav <yaniketofficial@gmail.com>
2026-04-23 14:54:55 +0000

    Configured Slint and made a small window :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
-#include"linklist.hpp"
+// #include"linklist.hpp"
+#include"app-window.h"
 using namespace std;
 
 int main() {
-
-    return 0;
+    auto hello_world = HelloWorld::create();
+    hello_world->set_my_label("UNGA BUNGA");
+    // Show the window and spin the event loop until the window is closed.
+    hello_world->run();
+    return 0;   
 }
