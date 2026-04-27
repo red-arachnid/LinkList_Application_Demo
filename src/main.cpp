97a048d733f2293af84efa5cd2380ef179125558
Aniket Yadav <yaniketofficial@gmail.com>
2026-04-27 09:30:25 +0000

    testing slint variables and functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 
 int main() {
     auto app = AppWindow::create();
+    int counter = app->get_counter();
+
+    app->on_test([&]() {
+        counter++;
+        app->set_counter(counter);
+    });
+
     app->run();
     return 0;
 }
